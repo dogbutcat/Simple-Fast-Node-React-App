3c24762be18e69269c11822e26405757259b710c
dogbutcat <dogbutcat@hotmail.com>
2017-02-22 20:51:44 +0800

    DELETE:deprecate old route and server code

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,7 +4,7 @@ import App from './app';
 import { PublicTimeline } from '../modules/Status';
 let rootEl = document.getElementById('react-app');
 var request = new XMLHttpRequest();
-request.open('GET', '/index.json', true);
+request.open('GET', '/json', true);
 request.onload = () => {
     if (request.status >= 200 && request.status < 400)
         rendeDOM(JSON.parse(request.responseText) as PublicTimeline);
